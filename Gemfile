c7142f1d6993b8bfef866bf1f80676697585a022
Joseph Haig <josephhaig@gmail.com>
2018-03-24 12:41:43 +0000

    Add explosions

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'rtanque', path: '../../awilliams/RTanque'
-#gem 'rtanque', git: 'https://github.com/awilliams/RTanque', tag: '0.2'
+gem 'rtanque', git: 'https://github.com/awilliams/RTanque', tag: '0.2'
 gemspec
--- lib/rtanque/data.rb
+++ lib/rtanque/data.rb
@@ -6,20 +6,44 @@ module RTanque
 
     def run
       @match_data = []
+      old_bot_list = @match.bots.map { |bot| [bot.object_id, []] }.to_h
+      explosions = {}
       while !@match.finished?
         @match.tick
+
+        live_bots = @match.bots.map do |bot|
+          [
+            bot.object_id,
+            {
+              name: bot.name,
+              x: bot.position.x,
+              y: bot.position.y,
+              health: bot.health,
+              heading: bot.heading.to_degrees.to_f,
+              turret: bot.turret.heading.to_degrees.to_f,
+              radar: bot.radar.heading.to_degrees.to_f
+            }
+          ]
+        end.to_h
+
+        (old_bot_list.keys - live_bots.keys).each do |bot_id|
+          explosions[bot_id] = {
+            x: old_bot_list[bot_id][:x],
+            y: old_bot_list[bot_id][:y],
+            explosion: 0
+          } if explosions[bot_id].nil?
+        end
+
+        explosions.keys.each do |bot_id|
+          explosions[bot_id][:explosion] += 1
+          explosions.delete(bot_id) if explosions[bot_id][:explosion] > 70
+        end
+
+        old_bot_list = live_bots
+
         @match_data << {
-          bots: @match.bots.map do |bot|
-                    {
-                      name: bot.name,
-                      x: bot.position.x,
-                      y: bot.position.y,
-                      health: bot.health,
-                      heading: bot.heading.to_degrees.to_f,
-                      turret: bot.turret.heading.to_degrees.to_f,
-                      radar: bot.radar.heading.to_degrees.to_f
-                    }
-                  end,
+          bots: live_bots.values,
+          explosions: explosions.values,
           shells: @match.shells.map do |shell|
                     { x: shell.position.x, y: shell.position.y }
                   end
